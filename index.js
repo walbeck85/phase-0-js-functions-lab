13496d51eab13d9c2326c8f6e4fc196bc47d94de
Will Albeck <will@albeck.me>
2025-07-12 03:15:32 -0400

    Implement all required functions for Functions in JavaScript lab

--- index.js
+++ index.js
@@ -1,7 +1,33 @@
+function calculateTax(amount) {
+  return amount * 0.10;
+}
 
+function convertToUpperCase(text) {
+  return text.toUpperCase();
+}
 
+function findMaximum(num1, num2) {
+  return num1 > num2 ? num1 : num2;
+}
 
+function isPalindrome(word) {
+  const normalized = word.toLowerCase();
+  return normalized === normalized.split('').reverse().join('');
+}
 
+function calculateDiscountedPrice(originalPrice, discountPercentage) {
+  const discount = originalPrice * (discountPercentage / 100);
+  return originalPrice - discount;
+}
+
+// Export functions if needed by tests
+module.exports = {
+  calculateTax,
+  convertToUpperCase,
+  findMaximum,
+  isPalindrome,
+  calculateDiscountedPrice
+};
 
 // This is required for the test to function properly  
 module.exports = { calculateTax, convertToUpperCase, findMaximum, isPalindrome, calculateDiscountedPrice };
